f78287b4c918a1d9e8da03e6dfcc4ef937192fe8
AhmadRiza <riza.public@gmail.com>
2023-10-22 21:38:56 +0700

    Move to https

--- app/src/main/java/com/weatheriza/core/network/HostUrl.kt
+++ app/src/main/java/com/weatheriza/core/network/HostUrl.kt
@@ -4,6 +4,6 @@ package com.weatheriza.core.network
  * Created by ahmadriza on 14/08/22.
  */
 object HostUrl {
-    const val OPEN_WEATHER_BASE_URL = "http://api.openweathermap.org/"
+    const val OPEN_WEATHER_BASE_URL = "https://api.openweathermap.org/"
     const val OPEN_WEATHER_ICON_URL = "https://openweathermap.org/img/wn/%s@4x.png"
 }
